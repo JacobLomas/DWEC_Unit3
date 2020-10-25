668ef3396b686bcb08d6914d1bff8260e04f8aef
JacobLomas <73339199+JacobLomas@users.noreply.github.com>
2020-10-25 13:23:16 +0100

    Update index.js

--- WorkSheet3/index.js
+++ WorkSheet3/index.js
@@ -50,54 +50,6 @@ class BookList{
         this.currentBook.setRead();
     }   
 }; */
-/* class Peon{
-    constructor(fila, columna){
-        this.nombre="peon";
-        this.fila=fila;
-        this.columna=columna;
-    }
-    mover(){
-        this.fila+=1;
-    }
-}
- class Player{
-    constructor(){
-        this.fichas=new Array(16).fill(true);
-    }
-}
-class Board{
-    constructor(){
-        this.rellenarTablero();
-    }
-    rellenarTablero(){ //He intentado hacer esta funcion con foreach pero esta es la unica manera en la que lo he conseguido :(
-        this.tablero=new Array(8);
-        for (var i = 0; i <8; i++) {
-            this.tablero[i] = new Array(8);
-            for(var j=0; j<8; j++){
-                if(i==6 || i==1)
-                this.tablero[i][j]=new Peon(i,j);
-                else
-                    this.tablero[i][j]=true;
-            }
-        }
-    }
-    moverPeon(i,j){
-        this.tablero[i][j].mover(i-1,j);
-        this.tablero[i-1][j]=this.tablero[i][j];
-        this.tablero[i][j]=false;
-    }
-}
-class Chess{
-    constructor(){
-        this.teamWhite=new Player();
-        this.teamBlack=new Player();
-        this.board=new Board();
-    }
-
-} 
-
-var ajedrez=new Chess();
- */
 class Player{
     constructor(name){
         this.name=name;
