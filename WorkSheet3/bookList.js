d6d38e59c2960bdc159b4e8b9dc35823938e75ee
JacobLomas <73339199+JacobLomas@users.noreply.github.com>
2020-11-05 13:09:09 +0100

    Update bookList.js

--- WorkSheet3/bookList.js
+++ WorkSheet3/bookList.js
@@ -52,14 +52,12 @@ class BookList{
     }   
 };
 var lista=new BookList();
-function main(){
+function main(){    
     //Añadimos las etiquetas para la caja de añadir libro
-    document.body.innerHTML='<div id="principal" style="border: 1px solid black; padding:20px; width:800px; height:800px;"> <div style="border: 1px solid black; padding:20px; width:300px;"> <label for="titulo">Titulo</label><input type="text" name="titulo" placeholder="Título" id="titulo"> <br>            <label for="genero">Género</label>            <input type="text" name="genero" placeholder="Género" id="genero"><br>            <label for="autor">Autor</label>            <input type="text" name="autor" placeholder="Autor" id="autor"><br>            <button id="añadir">Añadir libro</button>    </div></div>';
+    document.body.innerHTML='<div id="principal" style="border: 1px solid black; padding:20px; width:800px; height:800px;"> <div style="border: 1px solid black; padding:20px; width:300px;"> <label for="titulo">Titulo</label><input type="text" name="titulo" placeholder="Título" id="titulo"> <br>            <label for="genero">Género</label>            <input type="text" name="genero" placeholder="Género" id="genero"><br>            <label for="autor">Autor</label>            <input type="text" name="autor" placeholder="Autor" id="autor"><br>            <button id="anadir">Añadir libro</button>    </div></div>';
     document.getElementById("principal").innerHTML+='<br><br><div id="mostrarTabla" style="border: 1px solid black; padding:20px;"><button onclick="mostrarTabla()" id="mostrar">Mostrar tabla</button><div>';
-    anadirLibro();
-};
-function anadirLibro(){
-    document.getElementById("añadir").addEventListener("click", function(){ 
+    document.getElementById("principal").innerHTML+='<div id="info" style="border: 1px solid black; padding:20px; position:absolute; left:420px; top:30px"></div>';
+    document.getElementById("anadir").addEventListener("click", function(){ 
         if(document.getElementById("titulo").value=="" || document.getElementById("genero").value=="" || document.getElementById("autor").value=="")
             alert("Rellena todos los campos para añadir un libro");
         else{
@@ -67,16 +65,21 @@ function anadirLibro(){
             document.getElementById("titulo").value="";
             document.getElementById("genero").value="";
             document.getElementById("autor").value="";
+            mostrarTabla();
         }
     });
+
+    //anadirLibro();
 };
+function anadirLibro(){
+    };
 function mostrarTabla(){
     var tabla='<br><br><table style="border: 1px solid black; padding:20px; width:100%;">';
     tabla+='<tr> <td>Título</td> <td>Género</td> <td>Autor</td>  <td>Leído</td> </tr>';    
     for(libro of lista.books)
         tabla+='<tr><td>'+libro.title+'</td><td>'+libro.genre+'</td><td>'+libro.author+'</td><td>'+libro.read+'</td>'; 
     document.getElementById("mostrarTabla").innerHTML='<button onclick="mostrarTabla()" id="mostrar">Actualizar tabla</button>'+tabla;
-    document.getElementById("principal").innerHTML+='<div id="info" style="border: 1px solid black; padding:20px; position:absolute; left:420px; top:30px"></div>';
+    
     var info='<label>Ultimo libro leído:'+ultimoLibroMostrar()+'</label>';
     info+='<br> <label>Siguiente libro para leer: '+siguienteLibroMostrar()+'</label>';
     info+='<br> <label>Libro que me estoy leyendo: '+actualLibroMostrar()+'</label>';
